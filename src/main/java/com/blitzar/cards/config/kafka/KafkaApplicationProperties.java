7e1f1f89e842417b2e39f8d1d483b85147bd77af
Jefferson Condotta <j.condotta@complianceandrisks.com>
2023-04-26 23:08:25 +0200

    Initial Commit

--- src/main/java/com/blitzar/cards/config/kafka/KafkaApplicationProperties.java
+++ src/main/java/com/blitzar/cards/config/kafka/KafkaApplicationProperties.java
@@ -3,5 +3,5 @@ package com.blitzar.cards.config.kafka;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.kafka")
-public record KafkaApplicationProperties(String cardApplicationTopic, String cardApplicationGroupId) {
+public record KafkaApplicationProperties(String cardApplicationTopic) {
 }
